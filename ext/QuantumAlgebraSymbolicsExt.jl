01a7ac6899d12f438bfa19c39c330e86506b4b8a
Johannes Feist <johannes.feist@gmail.com>
2024-06-17 17:02:05 +0200

    fix QuantumAlgebraSymbolicsExt to work on julia 1.9 with Symbolics.jl 5.14

--- ext/QuantumAlgebraSymbolicsExt.jl
+++ ext/QuantumAlgebraSymbolicsExt.jl
@@ -3,8 +3,10 @@ module QuantumAlgebraSymbolicsExt
 using QuantumAlgebra
 using Symbolics
 
-QuantumAlgebra.numstring(x::Num) = iscall(Symbolics.value(x)) && operation(Symbolics.value(x))===(+) ? "($(string(x)))" : string(x)
-QuantumAlgebra.numlatex(x::Num) = iscall(Symbolics.value(x)) && operation(Symbolics.value(x))===(+) ? Expr(:latexifymerge,"\\left(",x, "\\right)") : x
+_issum(x::Num) = (v = Symbolics.value(x); Symbolics.SymbolicUtils.iscall(v) && Symbolics.operation(v) === (+))
+
+QuantumAlgebra.numstring(x::Num) = _issum(x) ? "($(string(x)))" : string(x)
+QuantumAlgebra.numlatex(x::Num) = _issum(x) ? Expr(:latexifymerge,"\\left(",x, "\\right)") : x
 
 QuantumAlgebra.num_expr(s::Union{Num,Complex{Num}}) = Symbolics.toexpr(s)
 
